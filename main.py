aedf4adde0847b4fe454e92b37be43ef0c4a7e14
catehuang <s0350228@gmail.com>
2022-09-01 12:37:36 -0600

    create canvas

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
+window.config(pady=20, padx=20, bg="white")
+logo_img = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
+canvas.create_image(100, 100, image=logo_img)
+canvas.pack()
 
 
 
